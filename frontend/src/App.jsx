276ef42981d22aa3aeea0dab67933105a57bad2d
Andika Palian <andika12des@gmail.com>
2025-01-01 20:24:22 +0800

    login

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,19 @@
 import './App.css'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './Login/Login'
+import Register from './Register/Register';
 
 function App() {
-
+  const routes = (
+    <Router>
+      <Routes>
+        <Route path='/' exact element={<Login />} />
+        <Route path='/register' exact element={<Register />} />
+      </Routes>
+    </Router>
+  )
   return (
-    <>
-      <Login />
-    </>
+    <div>{routes}</div>
   )
 }
 
--- frontend/src/Login/Login.jsx
+++ frontend/src/Login/Login.jsx
@@ -1,39 +1,54 @@
-import React, { useState } from 'react'
-import PasswordInput from '../components/Input/PasswordInput'
-import {Link, useNavigate} from 'react-router-dom';
-import "./login.css"
+import React, { useState } from "react";
+import PasswordInput from "../components/Input/PasswordInput";
+import { Link, useNavigate } from "react-router-dom";
+import "./login.css";
 
 const Login = () => {
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("")
-    const [error, setError] = useState(false);
-    // const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(false);
+  // const navigate = useNavigate();
 
-    const handleLogin = (e) => {
-        e.preventDefault();
-        setError(false);
+  const handleLogin = (e) => {
+    e.preventDefault();
+    setError(false);
 
-        if (!email || !password) {
-            setError("Please enter email and password");
-        }
+    if (!email || !password) {
+      setError("Please enter email and password");
     }
+  };
   return (
-    <div className='login-container'>
+    <div className="login-container">
+      <div className="login-form">
         <form onSubmit={handleLogin}>
-            <h4 className='login'>Login</h4>
-            <input type="email" placeholder='Email' required className='email-login' value={email} onChange={(e) => setEmail(e.target.value)} />
-            <PasswordInput value={password} onChange={(e) => setPassword(e.target.value)} required />
-            {error && (
-                <p className='error-message'>{error}</p>
-            )}
-            <button type='submit' className='login-button'>Login</button>
-            <p>
-                Don&apos;t have an account?{" "}
-                <Link to="/register" className='register-link'>Register</Link>
-            </p>
+          <h4 className="login">Login</h4>
+          <input
+            type="email"
+            placeholder="Email"
+            required
+            className="email-login"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+          <PasswordInput
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            required
+          />
+          {error && <p className="error-message">{error}</p>}
+          <button type="submit" className="login-button">
+            Login
+          </button>
+          <p>
+            Don&apos;t have an account?{" "}
+            <Link to="/register" className="register-link">
+              Register
+            </Link>
+          </p>
         </form>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
