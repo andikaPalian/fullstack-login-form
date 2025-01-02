8616d45c42b7382f820d96181697f568f66cc44c
Andika Palian <andika12des@gmail.com>
2025-01-03 06:50:16 +0800

    frontend completed

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -2,6 +2,7 @@ import './App.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Login from './Login/Login'
 import Register from './Register/Register';
+import Home from './Home/Home';
 
 function App() {
   const routes = (
@@ -9,6 +10,7 @@ function App() {
       <Routes>
         <Route path='/' exact element={<Login />} />
         <Route path='/register' exact element={<Register />} />
+        <Route path='/home' exact element={<Home />} />
       </Routes>
     </Router>
   )
--- frontend/src/Register/Register.jsx
+++ frontend/src/Register/Register.jsx
@@ -1,9 +1,60 @@
-import React from 'react'
+import React, { useState } from "react";
+import { Link, useNavigate } from "react-router-dom";
+import PassswordInput from "../components/Input/PasswordInput";
+import "./register.css";
 
 const Register = () => {
-  return (
-    <div>Register</div>
-  )
-}
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [error, setError] = useState(false);
+  const navigate = useNavigate();
 
-export default Register
+  const handleRegister = (e) => {
+    e.preventDefault();
+    setError(false);
+    if (!username || !email || !password) {
+      setError("Please enter username, email and password");
+    }
+  };
+  return (
+    <div className="register-container">
+      <form onSubmit={handleRegister}>
+        <h4 className="register">Register</h4>
+        <input
+          type="text"
+          placeholder="username"
+          className="register-input"
+          value={username}
+          required
+          onChange={(e) => {
+            setUsername(e.target.value);
+          }}
+        />
+        <input
+          type="email"
+          placeholder="email"
+          className="register-input"
+          value={email}
+          required
+          onChange={(e) => {
+            setEmail(e.target.value);
+          }}
+        />
+        <PassswordInput
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+        {error && <p className="error-message">{error}</p>}
+        <button type="submit" className="register-button">Create Account</button>
+        <p className="login">
+          Already have an account?{" "}
+          <Link to="/" className="login-link">Login</Link>
+        </p>
+      </form>
+    </div>
+  );
+};
+
+export default Register;
